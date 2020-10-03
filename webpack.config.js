56effab8af4b6f157908b2cd0fc13eba78006277
nat-davydova <natti.davydova@gmail.com>
2020-10-03 13:59:16 +0300

    Add css files handling and build mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,34 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require("path");
 
 module.exports = {
-  entry: { index: path.resolve(__dirname, "src", "index.js") },
+  entry: {
+    app: path.resolve(__dirname, "src", "app.js"),
+    styles: path.resolve(__dirname, "src", "css/styles.css")
+  },
   output: {
     path: path.resolve(__dirname, "dist")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+         },
+          "css-loader"
+        ]
+      }
+    ]
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, "src", "index.html")
-    })
-  ]
+    }),
+    new MiniCssExtractPlugin({
+      filename: `styles/[name].css`
+   }),
+  ],
 };
