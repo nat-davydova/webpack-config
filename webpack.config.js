4bc63b7cac8082a8d1e34bc1e1ec4710839b7e1b
nat-davydova <natti.davydova@gmail.com>
2020-10-03 13:12:56 +0300

    Add html files handling, entry point and output point

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,14 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
+
+module.exports = {
+  entry: { index: path.resolve(__dirname, "src", "index.js") },
+  output: {
+    path: path.resolve(__dirname, "dist")
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "src", "index.html")
+    })
+  ]
+};
