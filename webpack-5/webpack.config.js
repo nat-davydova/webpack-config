97bea153e63883fccac8bc38ff440f502eb64945
nat-davydova <natti.davydova@gmail.com>
2021-01-05 15:02:30 +0300

    webpack 5 version: preps - add entry and output points

--- webpack-5/webpack.config.js
+++ webpack-5/webpack.config.js
@@ -1,3 +1,16 @@
-module.exports = {
+const path = require("path");
 
+const PATHS = {
+  src: path.join(__dirname, './src'),
+  dist: path.join(__dirname, './dist')
+}
+
+module.exports = {
+  entry:  {
+    app: `${PATHS.src}/scripts/app.js`
+  },
+  output:{
+    path: `${PATHS.dist}`,
+    filename: './scripts/[name].[fullhash].min.js'
+  },
 }
