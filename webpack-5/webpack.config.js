dc2af04b0119713fa8515393207bfc560f991d4b
nat-davydova <natti.davydova@gmail.com>
2021-01-05 16:16:53 +0300

    webpack 5 version: pug - install packages, apply clear plugin

--- webpack-5/webpack.config.js
+++ webpack-5/webpack.config.js
@@ -1,9 +1,15 @@
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
 const path = require("path");
+const fs = require("fs");
 
 const PATHS = {
   src: path.join(__dirname, './src'),
   dist: path.join(__dirname, './dist')
 }
+
+const PAGES_PUG = `${PATHS.src}/pug/`
+const PAGES_TO_CONVERT = fs.readdirSync(PAGES_PUG).filter(filename => filename.endsWith('.pug'))
 
 module.exports = {
   entry:  {
@@ -13,4 +19,12 @@ module.exports = {
     path: `${PATHS.dist}`,
     filename: './scripts/[name].[fullhash].min.js'
   },
+  module: {
+    rules: [
+
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+  ]
 }
